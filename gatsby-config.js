536549ad6debee1247347a1e23d128b8b065bcbf
Processton <68786248+processton@users.noreply.github.com>
2023-09-07 22:07:53 +0000

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify`,
       options: {
-        mergeLinkHeaders: false, // boolean to turn off the default gatsby js headers
         mergeCachingHeaders: false, // boolean to turn off the default caching headers
         headers: {
-          "/schema/**/": ["Access-Control-Allow-Origin:*"],
-          "/schema/*/": ["Access-Control-Allow-Origin:*"],
-          "/schema/**": ["Access-Control-Allow-Origin:*"],
-          "/schema/*": ["Access-Control-Allow-Origin:*"],
           "/schema/": ["Access-Control-Allow-Origin:*"],
         },
       },
